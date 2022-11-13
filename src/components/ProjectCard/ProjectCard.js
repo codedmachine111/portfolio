a1917e8270fa2e2caf5e8fdb6ae9bb994fdd722e
codedmachine111 <charlesbabbage1709@gmail.com>
2022-11-13 22:49:35 +0530

    Added DEMO button for projects

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,11 +1,11 @@
 import "./ProjectCard.scss";
 import { Button } from "../Button/Button";
- 
+
 export const ProjectCard = (props) => {
   return (
     <>
       {!props.horz ? (
-          <div className="project-card ">
+        <div className="project-card ">
           <img src={props.img} alt="project" id={props.id} />
           <h2>{props.name}</h2>
           <p>{props.desc}</p>
@@ -13,9 +13,11 @@ export const ProjectCard = (props) => {
             <a href={`https://github.com/codedmachine111/${props.name}`} target="_blank">
               <Button title="GITHUB" />
             </a>
+            <a href={props.link} target="_blank">
+              <Button title="DEMO" />
+            </a>
           </div>
         </div>
-        
       ) : (
         <div className="project-card-horz">
           <img src={props.img} alt="project" id={props.id} />
@@ -24,6 +26,9 @@ export const ProjectCard = (props) => {
           <div className="btn-holder">
             <a href={`https://github.com/codedmachine111/${props.name}`} target="_blank">
               <Button title="GITHUB" />
+            </a>
+            <a href={props.link} target="_blank">
+              <Button title="DEMO" />
             </a>
           </div>
         </div>
